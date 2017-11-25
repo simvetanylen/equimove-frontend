c53e0d34cf0fca81554f8bfa62f4038f63fed7f2
simvetanylen <oreste.viron@gmail.com>
2017-11-25 13:38:59 +0100

    Changement url serveur

--- src/app/services/horse.service.ts
+++ src/app/services/horse.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const API_URL = "";
+const API_URL = "http://127.0.0.1:8080/equimove/api";
 
 @Injectable()
 export class HorseService {
